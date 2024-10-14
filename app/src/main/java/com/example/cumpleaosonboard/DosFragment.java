821f9261fd2bd2bf24508a166787be69233ac9e6
alejobasi <alejo3@educarex.es>
2024-10-14 21:26:19 +0200

    aa

---
+++ app/src/main/java/com/example/cumpleaosonboard/DosFragment.java
@@ -0,0 +1,37 @@
+package com.example.cumpleaosonboard;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link DosFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class DosFragment extends Fragment {
+
+
+    public DosFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dos, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/cumpleaosonboard/UnoFragment.java
@@ -0,0 +1,63 @@
+package com.example.cumpleaosonboard;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.cumpleaosonboard.databinding.FragmentUnoBinding;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the  factory method to
+ * create an instance of this fragment.
+ */
+public class UnoFragment extends Fragment {
+
+NavController navController;
+FragmentUnoBinding binding;
+    public UnoFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+       // return inflater.inflate(R.layout.fragment_uno, container, false);
+        binding= FragmentUnoBinding.inflate(getLayoutInflater());
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController= Navigation.findNavController(view);
+
+        binding.siguiente1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                navController.navigate(R.id.action_unoFragment_to_dosFragment);
+            }
+        });
+
+    }
+}
